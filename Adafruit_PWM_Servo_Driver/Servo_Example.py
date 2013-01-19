a1cdec39024d4457172a80a036abcdc12e53d0f3
Koen Kooi <koen@dominion.thruhere.net>
2013-01-19 09:24:08 +0000

    Servo_Example: adjust to Hexy hexapod
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- Adafruit_PWM_Servo_Driver/Servo_Example.py
+++ Adafruit_PWM_Servo_Driver/Servo_Example.py
@@ -2,35 +2,89 @@
 
 from Adafruit_PWM_Servo_Driver import PWM
 import time
+import math
 
-# ===========================================================================
-# Example Code
-# ===========================================================================
-
-# Initialise the PWM device using the default address
-# bmp = PWM(0x40, debug=True)
+# Controller at i2c address 0x40
 pwm = PWM(0x40, debug=True)
 
-servoMin = 150  # Min pulse length out of 4096
-servoMax = 600  # Max pulse length out of 4096
+# Store offsets, cheap analog servos aren't that precise
+servoCenter = [410,430,395,360,390,370,370,380,380,360,390,400,480,370,380,370]
+servoMin = [170,185,150,135,155,140,140,140,140,135,150,160,220,135,145,140]
+servoMax = [650,660,650,620,640,620,640,630,640,610,635,635,670,630,645,645]
 
-def setServoPulse(channel, pulse):
-  pulseLength = 1000000                   # 1,000,000 us per second
-  pulseLength /= 60                       # 60 Hz
-  print "%d us per period" % pulseLength
-  pulseLength /= 4096                     # 12 bits of resolution
-  print "%d us per bit" % pulseLength
-  pulse *= 1000
-  pulse /= pulseLength
-  pwm.setPWM(channel, 0, pulse)
+# Feet ID, counter-clockwise
+LF = 0
+LM = 1
+LB = 2
+RB = 3
+RM = 4
+RF = 5
 
 pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
-while (True):
-  # Change speed of continuous servo on channel O
-  pwm.setPWM(0, 0, servoMin)
-  time.sleep(1)
-  pwm.setPWM(0, 0, servoMax)
-  time.sleep(1)
 
+def setAngle(pwm, channel, angle):
+  if angle < -90:
+    angle = -90
+  if angle > 90:
+    angle = 90
+  if angle == 0:
+    pwmvalue = servoCenter[channel]
+  if angle > 0:
+    pwmvalue = servoCenter[channel] + (angle/90.0)*abs(servoMax[channel] - servoCenter[channel])
+  if angle < 0:
+    pwmvalue = servoCenter[channel] + (angle/90.0)*abs(servoCenter[channel] - servoMin[channel]) 
+  pwm.setPWM(channel, 0, int(pwmvalue))
+  #print "angle: %s, pwmvalue: %s" % (angle, pwmvalue)
 
+def setFootY(pwm, footno, ypos):
+  kneeAngle = math.degrees(math.asin(float(ypos)/85.0))
+  ankleAngle = 90.0-kneeAngle
+  setAngle(pwm,footno*2+1,kneeAngle)
+  setAngle(pwm,footno*2,-ankleAngle)
 
+for hip in [12, 13, 14, 15]:
+  setAngle(pwm,hip,0)
+
+for iter in range(1):
+  for i in range(15):
+    for leg in range(6):
+      setFootY(pwm,leg,-10 + i*5)
+    time.sleep(1/60.0)
+
+  for i in range(15):
+    for leg in range(6):
+      setFootY(pwm,leg,65-i*5)
+    time.sleep(1/60.0)
+
+time.sleep(1)
+
+setFootY(pwm,0,85)
+setFootY(pwm,1,50)
+setFootY(pwm,2,-10)
+setFootY(pwm,3,-10)
+setFootY(pwm,4,50)
+setFootY(pwm,5,85)
+
+time.sleep(0.5)
+
+setFootY(pwm,0,-15)
+setAngle(pwm,12,-60)
+setAngle(pwm,0,-60)
+time.sleep(0.2)
+setAngle(pwm,0,60)
+time.sleep(0.2)
+setAngle(pwm,0,-60)
+time.sleep(0.2)
+setAngle(pwm,0,60)
+
+time.sleep(5)
+
+setAngle(pwm,12,0)
+
+for leg in range(6):
+  setFootY(pwm,leg,-10)
+
+time.sleep(0.5)
+
+for i in range(16):
+  pwm.setPWM(i,0,0)
