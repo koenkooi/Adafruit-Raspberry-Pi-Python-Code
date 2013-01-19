6ffc252122fb9ebee2055ec449cbbebfb5ccc365
Koen Kooi <koen@dominion.thruhere.net>
2013-01-19 09:23:45 +0000

    Adafruit_PWM_Servo_Driver: add missing import, adjust for beaglebone bus numbering
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- Adafruit_PWM_Servo_Driver/Adafruit_PWM_Servo_Driver.py
+++ Adafruit_PWM_Servo_Driver/Adafruit_PWM_Servo_Driver.py
@@ -2,6 +2,7 @@
 
 import time
 import math
+import smbus
 from Adafruit_I2C import Adafruit_I2C
 
 # ============================================================================
@@ -27,7 +28,7 @@ class PWM :
   __ALLLED_OFF_H       = 0xFD
 
   def __init__(self, address=0x40, debug=False):
-    self.i2c = Adafruit_I2C(address)
+    self.i2c = Adafruit_I2C(address, bus=smbus.SMBus(1))
     self.address = address
     self.debug = debug
     if (self.debug):
